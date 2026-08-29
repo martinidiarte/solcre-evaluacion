aa37d17a83cf671ca7b3b77ca9d0dc7bea85a986
Martin Idiarte <martinidiarte123@gmail.com>
2026-08-29 11:34:12 -0300

    Configuracion de Docker y conexion del backend con MySQL

---
+++ backend/database.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy import text
+import os
+
+#Obtengo las variables de entorno para la conexión a la base de datos MySQL
+host = os.getenv("MYSQL_HOST")
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+database = os.getenv("MYSQL_DATABASE")
+port = int(os.getenv("MYSQL_PORT"))
+
+#El formato que espera SQLAlchemy con PyMySQL es, conceptualmente:
+# mysql+pymysql://USUARIO:CONTRASEÑA@HOST:PUERTO/BASE_DE_DATOS
+
+#Idea inicial, pero no es la forma más robusta de construir la URL de conexión
+#database_url = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+
+#Mas robusto
+database_url = URL.create(
+    drivername="mysql+pymysql",
+    username=user,
+    password=password,
+    host=host,
+    port=port,
+    database=database
+)
+
+#Creo el motor de SQLAlchemy para conectarme a la base de datos MySQL
+engine = create_engine(database_url, echo=True)  # echo=True para ver las consultas SQL en la consola   
+
+# El bloque 'with' asegura que la conexión se cierre automáticamente
+# 1. Abrimos la conexión
+with engine.connect() as connection:
+    # 2. Ejecutamos una consulta usando text()
+    resultado = connection.execute(text("SELECT 'Hola, mundo'"))
+    
+    # 3. Leemos los datos
+    for fila in resultado:
+        print(fila)
